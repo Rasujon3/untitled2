09ab27b51e24b4d61e78cf76945eafdc585f65d7
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-23 21:54:26 +0600

    58 React Native Wix Navigation "Component layout use"
    58 React Native Wix Navigation "Component layout use"

--- Home.js
+++ Home.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 class Home extends Component {
+
+  constructor(props) {
+    super();
+  }
+
   render() {
     return (
       <View>
-        <Text style={{fontSize: 40}}>This is Home Page</Text>
+        <Text style={{fontSize: 40}}>{this.props.pageTitle}</Text>
+        <Text style={{fontSize: 20}}>{this.props.pageSubTitle}</Text>
       </View>
     );
   }
--- index.js
+++ index.js
@@ -3,16 +3,24 @@ import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 
-Navigation.registerComponent('HomePage',()=>Home);
-Navigation.registerComponent('AboutPage',()=>About);
-Navigation.registerComponent('ContactPage',()=>Contact);
+Navigation.registerComponent('HomePage', () => Home);
+Navigation.registerComponent('AboutPage', () => About);
+Navigation.registerComponent('ContactPage', () => Contact);
 
-Navigation.events().registerAppLaunchedListener(()=>{
+const component = {
+    id: "homeID",
+    name: 'HomePage',
+    options: {},
+    passProps: {
+        pageTitle: "Home Page Title",
+        pageSubTitle: "Home Page subtitle",
+    }
+}
+
+Navigation.events().registerAppLaunchedListener(() => {
     Navigation.setRoot({
-        root:{
-            component:{
-                name:'ContactPage'
-            }
+        root: {
+            component
         }
     });
 })
